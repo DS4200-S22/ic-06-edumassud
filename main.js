5cfc098c84fddd2235abe4afa39d5e2a155a10c3
edumassud <58275179+edumassud@users.noreply.github.com>
2022-02-10 13:18:34 -0500

    Done
    Button 2 doesn't work on first click.
    Prof said it's prob a Chrome issue

--- main.js
+++ main.js
@@ -10,5 +10,16 @@ Modified: 12/06/21
     // You will need to call this function when Button 1 is clicked
     // You will need to modiffy the body of this function as described in the assignment Readme
 function button1Clicked() {
-    console.log("Button 1 was clicked"); 
+    let random_num = 100 * Math.random().toFixed(2);
+    document.getElementById("random_num").innerHTML = random_num;
+}
+
+function button2Clicked() {
+    console.log(document.getElementById("circle").style.fill);
+    if (document.getElementById("circle").style.fill == "blue") {
+        document.getElementById("circle").style.fill = "green";
+    }
+    else {
+        document.getElementById("circle").style.fill = "blue";
+    }
 }
